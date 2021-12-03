ccd9bbefd6e2ab500973e6bd25ba5c74bbe52221
Axhar85 <azhararshad_ntu@yahoo.com>
2021-12-03 15:32:52 +0100

    created a put route to update

--- app/routes/note_route.js
+++ app/routes/note_route.js
@@ -25,8 +25,21 @@ module.exports = function(app, db) {
         });
     });
 
+    app.put('./notes/:id', (req, res) => {
+        const id = req.params.id;
+        const detailes = {'_id': new ObjectID(id)};
+        const note = {text: req. body.body, title: req.body.title};
+        db.collection('notes').update(detailes, note,  (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(item)
+            }
+        });
+    });
+
     app.post('/notes', (req, res) => {
-        const note = {text: req. body.body, title: req.body.title}
+        const note = {text: req. body.body, title: req.body.title};
         db.collection('notes').insert(note, (err, result) =>{
             if (err) {
                 res.send({'error': 'An error has occured'});
