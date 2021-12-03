da52bbe259be7cf2dbc7fba3f25b57f51544b4e3
Axhar85 <azhararshad_ntu@yahoo.com>
2021-12-03 15:17:00 +0100

    creating a get route

--- app/routes/note_route.js
+++ app/routes/note_route.js
@@ -1,4 +1,14 @@
 module.exports = function(app, db) {
+    app.get('./notes/:id', (req, res => {
+        const detailes = {'_id': <ID GOES HERE>};
+        db.collection('notes').findOne(detailes, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(item)
+            }
+        })
+    })) 
     app.post('/notes', (req, res) => {
         const note = {text: req. body.body, title: req.body.title}
         db.collection('notes').insert(note, (err, result) =>{
