4d63b5652d45120c2f8cec31825dee9f795db912
Axhar85 <azhararshad_ntu@yahoo.com>
2021-12-03 15:24:29 +0100

    passing mongodb and fixed error

--- app/routes/note_route.js
+++ app/routes/note_route.js
@@ -1,14 +1,17 @@
+var ObjectID = require('mongodb').ObjectID
+
 module.exports = function(app, db) {
-    app.get('./notes/:id', (req, res => {
-        const detailes = {'_id': <ID GOES HERE>};
+    app.get('./notes/:id', (req, res) => {
+        const id = req.params.id;
+        const detailes = {'_id': new ObjectID(id)};
         db.collection('notes').findOne(detailes, (err, item) => {
             if (err) {
                 res.send({'error': 'An error has occured'});
             } else {
                 res.send(item)
             }
-        })
-    })) 
+        });
+    });
     app.post('/notes', (req, res) => {
         const note = {text: req. body.body, title: req.body.title}
         db.collection('notes').insert(note, (err, result) =>{
